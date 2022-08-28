3231edcdcce8a8ad139725fcf17ba70a45b1f595
Meraj Rizwan <meraj.rizwan9832@gmail.com>
2022-08-28 23:29:52 +0530

    digital-clock-html-css-mini-project-NS

---
+++ App.js
@@ -0,0 +1,25 @@
+const clock = document.querySelector(".clock");
+
+setInterval(function() {
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hr = time.getHours();
+    let day = 'AM';
+
+    if (hr == 0) {
+        hr = 12;
+    }
+    if (hr > 12) {
+        day = 'PM';
+        hr = hr - 12;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    if (hr < 10) {
+        hr = "0" + hr;
+    }
+
+    clock.textContent = hr + ':' + min + ':' + sec + " " + day;
+});
